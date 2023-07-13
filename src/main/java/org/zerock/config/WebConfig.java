75c879c5a3e5bfe3a852350b969d6a32a4f37b82
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-13 17:34:30 +0900

    WebConfig 생성하여 AbstractAnnotationConfigDispatcherServletInitializer 추상클래스 상속, getRootConfig() 클래스를 root-context.xml 대신 사용, getRootConfigClasses()의 메서드 내용 변경

---
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,26 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() { //root-context.xml을 대신함
+		return new Class[] {
+				RootConfig.class
+				};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
