7a623dcc0c5fdfbee53e0bee00a4a684cc8f19c0
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-15 19:58:09 +0900

    자바를 이용하는 경우의 테스트 설정, 오류 확인

---
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -0,0 +1,10 @@
+package org.zerock.sample;
+
+import org.zerock.config.RootConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {RootConfig.class})
+@Log4j
+public class SampleTests {
+
+}
