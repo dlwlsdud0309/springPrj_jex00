135c28886c349b0f633af8ca16e7fb12b9f4a1fe
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-18 22:53:39 +0900

    @Component 어노테이션에 대하여

--- src/test/java/org/zerock/sample/SampleTests.java
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -8,7 +8,7 @@ import org.zerock.config.RootConfig;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= {RootConfig.class})
+@ContextConfiguration(classes= {RootConfig.class}) //지정된 클래스나 문자열을 이용해서 필요한 객체들을 스프링 내에 객체(스프링의 빈)로 등록
 @Log4j
 public class SampleTests {
 
