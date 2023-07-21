dc44fc0685794023f19636d34308758e0fab0552
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-21 15:05:41 +0900

    SampleTests 오류(An illegal reflective access operation has occurred) 확인

--- src/test/java/org/zerock/sample/SampleTests.java
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -14,7 +14,9 @@ import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes= {RootConfig.class}) //지정된 클래스나 문자열을 이용해서 필요한 객체들을 스프링 내에 객체(스프링의 빈)로 등록
-@Log4j
+//지정된 클래스나 문자열을 이용해서 필요한 객체들을 스프링 내에 객체(스프링의 빈)로 등록
+//스프링이 실행되면서 어떤 설정 정보를 읽어 들여야 하는지를 명시
+@Log4j //Lombok을 이용해서 로그를 기록하는 Logger를 변수로 생성
 public class SampleTests {
 	
 	@Setter(onMethod_ = {@Autowired}) //해당 인스턴스 변수가 스프링으로부터 자동으로 주입해 달라는 표시
